669aa2008c67749568dee8fe3f279ec617d3922c
Nadia Kane <nadiakane@Nadias-MacBook-Air.local>
2024-12-02 19:08:23 -0600

    Added Sign In/Out/Up to Nav Bar

--- db/schema.rb
+++ db/schema.rb
@@ -23,10 +23,10 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_02_204752) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "stripe_customer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 end
