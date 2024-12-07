3252cfe1708378aaf5a5e3decbd6f0db05bb2661
Nadia Kane <nadiakane@Nadias-MacBook-Air.local>
2024-12-06 23:42:13 -0600

    Commits controller Add and Delete

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_landmark
+
+    def create
+      comment = @landmark.comments.create(comment_params.merge(user_id: current_user.id))
+      redirect_to landmark_path(@landmark)
+    end
+  
+    def destroy
+      @comment = @landmark.comments.find(params[:id])
+      @comment.destroy
+      redirect_to landmark_path(@landmark), status: :see_other
+    end
+
+    private
+    
+    def set_landmark
+        @landmark = Landmark.find(params[:landmark_id])
+    end
+
+      def comment_params
+        params.expect(comment: [:body, :user_id, :landmark_id])
+      end
+  end
--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -1,6 +1,7 @@
 class Landmark < ApplicationRecord
   belongs_to :user
   belongs_to :region
+  has_many :comments
 
   geocoded_by :address
   after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
