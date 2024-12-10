43a1b0b70fda1c14041ecc830ed9f944891a752e
Nadia Kane <nadiakane@Nadias-MacBook-Air.local>
2024-12-09 19:45:13 -0600

    Color by region

--- app/helpers/regions_helper.rb
+++ app/helpers/regions_helper.rb
@@ -1,2 +1,12 @@
 module RegionsHelper
+    def region_button_color(region)
+        case region.name.downcase
+        when "northeast" then "btn-outline-primary" # Blue
+        when "northwest" then "btn-outline-success" # Green
+        when "metro" then "btn-outline-danger"   # Red
+        when "southwest" then "btn-outline-warning"  # Yellow
+        when "southeast" then "btn-outline-secondary"  # Gray
+        when "central" then "btn-outline-info"  # Turquois
+        end
+      end
 end
