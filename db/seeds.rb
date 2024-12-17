37d901a9ded10eb4d2d49584c87ab79721cbf500
Nadia Kane <nadiakane@Nadias-MacBook-Air.local>
2024-12-17 09:22:31 -0600

    Heroku database url production database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Generate new users
+10.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  username = "#{first_name.downcase}#{last_name.downcase}"
+  email = "#{first_name.downcase}.#{last_name.downcase}@example.com"
+
+  User.create!(
+    first_name: first_name,
+    last_name: last_name,
+    username: username,
+    email: email,
+    password: "password",
+    password_confirmation: "password"
+  )
+end
+
+puts "Seeding completed successfully."
