3b0abbed729d278bed7d042d6fc7fbda8df53b96
Nadia Kane <nadiakane@Nadias-MacBook-Air.local>
2025-01-12 10:23:16 -0600

    Admin clean up for Regions with flash notice and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to regions_path  # Redirect to a suitable page (e.g., the regions index)
+  end
+
   protected
 
   def configure_permitted_parameters
